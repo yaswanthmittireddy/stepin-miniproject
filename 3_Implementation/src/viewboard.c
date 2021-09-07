2253019ae4af5119853c2750edd2302500603519
yaswanth kumar <82078258+yaswanthmittireddy@users.noreply.github.com>
2021-09-07 16:00:50 +0530

    Add files via upload

---
+++ 3_Implementation/src/viewboard.c
@@ -0,0 +1,20 @@
+#include "functions.h"
+#include <stdio.h>
+#include <conio.h>
+
+void board(char *square)
+{
+    
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+    printf("_||__\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+    printf("_||__\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+    printf("     |     |     \n\n");
+}
